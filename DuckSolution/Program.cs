b261f0a4225faa67b06ce98f9b3f1d25d2490b53
ShahFire <shah-fire@mail.ru>
2022-02-18 16:11:52 +0300

    Добавил ссылку на библиотеку классов и задействовал классы в головном файле

--- DuckSolution/Program.cs
+++ DuckSolution/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DuckLibrary;
 
-namespace DuckSolution
+namespace DuckProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Duck Bob = new MallardDuck();
+            Duck Mac = new RedheadDuck();
+
+            Duck[] ducks = { Bob, Mac };
+
+            for (int i = 0; i < ducks.Length; i++)
+            {
+                Console.WriteLine(ducks[i].Quack());
+                Console.WriteLine(ducks[i].Swim());
+                Console.WriteLine(ducks[i].Display());
+            }
+
+            Console.ReadKey();
         }
     }
 }
