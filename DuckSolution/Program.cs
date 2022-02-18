ffba3286f9ae4ae243db0f280795cec328a00f4a
ShahFire <shah-fire@mail.ru>
2022-02-18 16:37:44 +0300

    Написал новый головной файл с участием новых классов и интерфесов

--- DuckSolution/Program.cs
+++ DuckSolution/Program.cs
@@ -11,16 +11,27 @@ namespace DuckProject
     {
         static void Main(string[] args)
         {
-            Duck Bob = new MallardDuck();
-            Duck Mac = new RedheadDuck();
+            Duck Mark = new MallardDuck();
+            Duck Riley = new RedheadDuck();
+            Duck Ruby = new RubberDuck();
+            Duck Devid = new DecoyDuck();
 
-            Duck[] ducks = { Bob, Mac };
+            Duck[] ducks = { Mark, Riley, Ruby, Devid };
 
             for (int i = 0; i < ducks.Length; i++)
             {
-                Console.WriteLine(ducks[i].Quack());
                 Console.WriteLine(ducks[i].Swim());
                 Console.WriteLine(ducks[i].Display());
+                if (ducks[i] is Quackable)
+                {
+                    Console.WriteLine((ducks[i] as Quackable).Quack());
+                }
+
+                if (ducks[i] is Flyable)
+                {
+                    Console.WriteLine((ducks[i] as Flyable).Fly());
+                }
+
             }
 
             Console.ReadKey();
